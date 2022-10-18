cd24055312c294ff6e140c6c2114f12fba96b7e4
lautisaya <lautarosaya0012@gmail.com>
2022-10-18 15:59:58 -0300

    new ex added

--- clase-4/clase4-ej.js
+++ clase-4/clase4-ej.js
@@ -1,7 +1,7 @@
 /* DESAFIO 1 */
-const perfil = 'invitado';
+/* const perfil = '';
 
-/* switch (perfil) {
+switch (perfil) {
     case 'administrador':
         console.log('Usted tiene todos los privilegios de uso del sistema.');
         break;
@@ -13,10 +13,13 @@ const perfil = 'invitado';
     case 'invitado':
         console.log('Usted solo tiene permisos de consultar datos');
         break;
+    case '':
+        console.log('Debe especificar el perfil del usuario.');
+        break;
 
     default:
         console.log('Debe espesificar un perfil valido.');
-} */
+} */ 
 
 /* DESAFIO 2 */
 
@@ -28,32 +31,58 @@ let nuevoPago = consumoKWH > 300 ? pagoMes * 20 / 100 + pagoMes: console.log('Su
 
 nuevoPago > pagoMes ? console.log(`Debido a que su hogar ruvo un consumo de ${consumoKWH}KWH, en base al ajuste tarifario (hogares con consumo mayor a 300kwh por mes tendran un aumento del 20%),cumplimos con informarle que se ha ajustado el total a pagar, que sera de $${nuevoPago}`): ''; */
 
-/* DESAFIO 3 */
+/* DESAFIO  3*/
 
-const numUno = 1;
+/* const palabra = 'gato';
+
+switch (palabra) {
+    case 'gato':
+        console.log('cat');
+        break;
+    case 'perro':
+        console.log('dog');
+        break;
+    case 'puerta':
+        console.log('door');
+        break;
+    case 'ventana':
+        console.log('window');
+        break;
+    case 'mesa':
+        console.log('table');
+        break;
+    default:
+        console.log('La palabra ingresada no es correcta.');
+} */
+
+/* DESAFIO 4 */
+
+/* const numUno = 1;
 const numDos = 2;
-const operacion = 'resta';
+const operacion = 'division';
 let resultado;
+
 
 switch (operacion) {
     case 'suma':
         resultado = numUno + numDos;
-        console.log(`El resultado de la ${operacion} ${numUno} y ${numDos} es: ${resultado}`);
         break;
     case 'resta':
         resultado = numUno - numDos;
-        console.log(`El resultado de la ${operacion} ${numUno} y ${numDos} es: ${resultado}`);
         break;
     case 'multiplicacion':
         resultado = numUno * numDos;
-        console.log(`El resultado de la ${operacion} ${numUno} y ${numDos} es: ${resultado}`);
         break;
     case 'division':
         resultado = numUno / numDos;
-        console.log(`El resultado de  la${operacion} ${numUno} y ${numDos} es: ${resultado}`);
         break;
     default:
         console.log('No es una operacion valida.');
-}
+} 
+
+if (resultado != undefined) {
+    console.log(`El resultado de la ${operacion} ${numUno} y ${numDos} es: ${resultado}`);
+}  */
+
 
 

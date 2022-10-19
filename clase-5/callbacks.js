4ffa814faf776e166f8fbdcb43918ed94a58f6ae
lautisaya <lautarosaya0012@gmail.com>
2022-10-19 00:53:01 -0300

    ex in class 5 added

---
+++ clase-5/callbacks.js
@@ -0,0 +1,41 @@
+/* EJEMPLO DE VIDEO */
+
+/* funcion de suma */
+//let suma = (numUno, numDos) => numUno + numDos;
+/* funcion de resta */
+//let resta = (numUno, numDos) => numUno - numDos;
+/* funcion con operacion y el callback, seria "operacion", primero se pasa como parametro y despues lo pones como funcion */
+//let operacionMatematica = (numUno, numDos, operacion) => operacion(numUno, numDos);
+
+//console.log(operacionMatematica(5, 4, suma));
+
+/* EJ DOS VIDEO CALCULADORA*/
+
+/* let suma = (numUno, numDos) => numUno + numDos;
+let resta = (numUno, numDos) => numUno - numDos;
+let division = (numUno, numDos) => numUno / numDos;
+let multiplicacion = (numUno, numDos) => numUno * numDos;
+
+let calculadora = (numUno, numDos, operacion) => operacion(numUno, numDos);
+
+console.log(calculadora(2, 2, resta)); */
+
+/* EJ 1 PLAYGROUND */
+
+/* let doble = numUno => numUno * 2;
+let triple = numUno => numUno * 3;
+
+let aplicarCallBack = (numUno, operacion) => operacion(numUno);
+
+console.log(aplicarCallBack(2, triple)); */
+
+/* EJ 2 PLAYGROUND */
+
+/* let suma = (numUno, numDos) => numUno + numDos;
+let resta = (numUno, numDos) => numUno - numDos;
+let division = (numUno, numDos) => numUno / numDos;
+let multiplicacion = (numUno, numDos) => numUno * numDos;
+
+let calculadora = (numUno, numDos, operacion) => operacion(numUno, numDos);
+
+console.log(calculadora(2, 2, resta)); */

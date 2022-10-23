14d7fdb053c2618aa11c0bc34c73a838e912a08a
lautarosaya <lautarosaya0012@gmail.com>
2022-10-23 00:59:05 -0300

    class 6 add

---
+++ clase-6/arrays.js
@@ -0,0 +1,37 @@
+/* EJ 1 VIDEO */
+
+/* let seriesFavoritas = ['Rick and Morty', 'Dragon Ball', 'Hunter x Hunter'];
+
+console.log(seriesFavoritas[2]);
+
+ */
+
+/* EJ 2 VIDEO */
+
+/* let seriesFavoritas = ['Rick and Morty', 'Dragon Ball', 'Hunter x Hunter'];
+
+console.log(seriesFavoritas.length); */
+
+/* EJ 3 VIDEO */
+
+/* let seriesFavoritas = ['Rick and Morty', 'Dragon Ball', 'Hunter x Hunter'];
+
+console.log(seriesFavoritas); */
+
+/* EJ 4 VIDEO */
+/* array dentro de un array */
+/* let nombres = ['Lautaro', 'Belen', 'Camila', 'Rodrigo'];
+let mesas = [1, 2, 3, 4, nombres];
+
+console.log(mesas); */
+/* const lautaro = ['Lautaro', 'Sayavedra', 20]
+const belen = ['Belen', 'Franch', 21]
+const camila = ['Camila', 'Sayavedra', 17]
+const rodrigo = ['Rodrigo', 'Rivero', 19]
+
+let nombres = [lautaro, belen, camila, rodrigo];
+
+console.log(nombres); */
+
+
+
---
+++ clase-6/metodosArray.js
@@ -0,0 +1,146 @@
+/* EJ 1 PLAYGROUND join()*/
+/* let arrayFrase = [
+    "No",
+    "he",
+    "fracasado,",
+    "simplemente",
+    "me",
+    "he",
+    "topado",
+    "con",
+    "diez",
+    "mil",
+    "soluciones",
+    "equivocadas"
+  ];
+  
+  let fraseNueva = arrayFrase.join(' ');
+  console.log(fraseNueva); */
+
+  /* EJ 2 PLAYGROUND pop() /*   
+  let estudiantes = [
+    {
+      nombre: "Alvaro",
+      promedio: 9,
+      curso: "Android"
+    },
+    {
+      nombre: "Daniel",
+      promedio: 6,
+      curso: "Full Stack"
+    },
+    {
+      nombre: "Alexis",
+      promedio: 3,
+      curso: "iOS"
+    }
+  ];
+
+  const alumnoEgresado = estudiantes.pop();
+  console.log(alumnoEgresado); */
+
+  /* EJ 3 PLAYGROUND push() */
+
+/*   let estudiantes = [
+    {
+      nombre: 'Alvaro',
+      promedio : 9,
+      curso : 'Android',
+    },
+    {
+      nombre: 'Daniel',
+      promedio : 6,
+      curso : 'Full Stack',
+    },
+    {
+      nombre: 'Alexis',
+      promedio : 3,
+      curso : 'iOS',
+    },
+  ]
+
+estudiantes.push(
+    {
+        nombre: 'Juan',
+
+        promedio: 5,
+    
+        curso: 'iOS'
+    }
+)
+estudiantes.push(
+    {
+        nombre: 'Miguel',
+
+        promedio: 2,
+
+        curso: 'Android'
+    }
+)
+
+console.log(estudiantes); */
+
+/* EJ 4 PLAYGROUND shift() */
+
+/* let estudiantes = [
+    {
+       nombre: 'Alvaro',
+       promedio : 9,
+       curso : 'Android',
+     },
+      {
+        nombre: 'Daniel',
+        promedio : 6,
+        curso : 'Full Stack',
+      },
+      {
+        nombre: 'Alexis',
+        promedio : 3,
+        curso : 'iOS',
+      },
+]
+
+const alumnoDeBaja = estudiantes.shift();
+console.log(alumnoDeBaja); */
+
+/* EJ 5 PLAYGROUND unshift() */
+
+/* let estudiantes = [
+    {
+      nombre: 'Alvaro',
+      promedio : 9,
+      curso : 'Android',
+    },
+    {
+      nombre: 'Daniel',
+      promedio : 6,
+      curso : 'Full Stack',
+    },
+    {
+      nombre: 'Alexis',
+      promedio : 3,
+      curso : 'iOS',
+    }
+  ]
+
+estudiantes.unshift(
+    {
+        nombre: "Mariana",
+
+        promedio: 9,
+
+        curso: "Full Stack"
+    }
+)
+
+estudiantes.unshift(
+    {
+        nombre: "Francisco",
+
+        promedio: 2,
+
+        curso: "Android"
+    }
+)
+
+console.log(estudiantes); */

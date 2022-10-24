0a44d352227e7b62cbb6843d0ea6fb0523e77ac6
lautarosaya <lautarosaya0012@gmail.com>
2022-10-24 08:22:37 -0300

    ej class 6

--- clase-6/ejClase6.js
+++ clase-6/ejClase6.js
@@ -1,9 +1,39 @@
 const electrodomesticos = ['Microondas', 'Heladera', 'Cocina', 'Horno', 'Licuadora', 'Tostadora']
 
-/* Aleatorio */
+//*Aleatorio
 /* const aleatorio = electrodomesticos[Math.floor(Math.random() * electrodomesticos.length)];
 console.log(aleatorio); */
 
-const borrarAddFinal = electrodomesticos.shift();
+//*Sacar de inicio y poner al final
+/* const borrarAddFinal = electrodomesticos.shift();
 const addFinal = electrodomesticos.push(borrarAddFinal);
-console.log(addFinal);
+console.log(electrodomesticos); */
+
+//*Agregar dos elementos mas al array
+/* const addOne = electrodomesticos.push('Batidora');
+const addTwo = electrodomesticos.push('Lavavajillas');
+console.log(electrodomesticos); */
+
+//*Mostrar cantidad de elementos
+/* const cantidad = electrodomesticos.length;
+console.log(cantidad, electrodomesticos); */
+
+//*Buscar elemento en array y indicar si existe o no 
+/* const buscar = (array, elemento) => {
+    if (array.includes(elemento) == true) {
+        return 'Producto encontrado';
+    } else{
+        return 'El producto buscado no existe';
+    }
+}
+console.log(buscar(electrodomesticos, 'Frezeer')); */
+
+//* Juntar elementos de array con espaciado
+/* const juntarArray = electrodomesticos.join(' ');
+console.log(juntarArray);
+ */
+
+//* Determinar la cantidad de elementos que tiene el string obtenido
+/* const conversionArray = juntarArray.split(' ');
+const elementosArray = conversionArray.length
+console.log(conversionArray, elementosArray); */

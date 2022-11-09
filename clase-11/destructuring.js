54b04feb6ee9bea087e5589ea1a9ad54452774f0
lautisaya <lautarosaya0012@gmail.com>
2022-11-09 16:19:09 -0300

    class 11

---
+++ clase-11/destructuring.js
@@ -0,0 +1,33 @@
+//* con array- este se va a asignar dentro del array por orden
+/* let colores = ["negro", "rojo", "verde"];
+
+let [colorNegro, colorRojo, colorVerde] = colores;
+
+console.log(colorRojo);
+ */
+
+//* con objeto- este se asigna en el objeto por nombre espesifico
+/* let personas = {
+  nombre: "lautaro",
+  edad: 20,
+  profesion: "developer",
+  serieFavorita: "Menem",
+};
+
+let { nombre, edad } = personas; */
+
+//*Ej1 playground
+/* let destinosDelMundo = [
+  "Marruecos",
+  "Bariloche",
+  "Barcelona",
+  "China",
+  "Grecia",
+];
+
+let [marruecos, bariloche, barcelona, china, grecia] = destinosDelMundo;
+console.log(bariloche, china); */
+
+//*Ej 2 playground
+/* let auto = { marca: "Ferrari", kilometros: 31, color: "Rojo" };
+let { marca, color } = auto; */
